2167a4522ef1543fb3063cdca9907c1a9da38120
Defective <def3ctive4@gmail.com>
2026-04-27 22:29:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jtwitch-api
+
+A simple, partial implementation of Twitch API, focusing on chat, followers, and events
---
+++ src/main/java/io/github/defective4/twitch/api/Main.java
@@ -0,0 +1,9 @@
+package io.github.defective4.twitch.api;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
